c9bed58421f74b8ae239bc19038fcb493e13849b
Jace <54625750+Jacekun@users.noreply.github.com>
2020-12-18 15:27:58 +0800

    Created main GUI controls.

--- AnilistExport/frmMain.Designer.cs
+++ AnilistExport/frmMain.Designer.cs
@@ -28,20 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.btnExport = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(512, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter your Username, and Click on \'Export\'";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblUser
+            // 
+            this.lblUser.Location = new System.Drawing.Point(0, 109);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(148, 34);
+            this.lblUser.TabIndex = 1;
+            this.lblUser.Text = "Username: ";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(154, 109);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(346, 40);
+            this.txtUser.TabIndex = 2;
+            // 
+            // btnExport
+            // 
+            this.btnExport.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btnExport.ForeColor = System.Drawing.Color.Silver;
+            this.btnExport.Location = new System.Drawing.Point(94, 182);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(288, 58);
+            this.btnExport.TabIndex = 3;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = false;
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 33F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(620, 324);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(512, 267);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.MaximizeBox = false;
             this.Name = "frmMain";
             this.Text = "frmMain";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Button btnExport;
     }
 }
 
